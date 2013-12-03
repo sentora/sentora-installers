e8035f68fa5527f2feea64714df147bffde9b71f
Kimpe Andy <andykimpe@gmail.com>
2013-12-03 15:33:25 +0100

    Update 10_1_1.sh

--- install/Ubuntu-12_04/10_1_1.sh
+++ install/Ubuntu-12_04/10_1_1.sh
@@ -397,22 +397,22 @@ cd ../
 rm -rf zp_install_cache/ zpanelx/
 
 # Advise the user that ZPanel is now installed and accessible.
-echo -e "##############################################################"
-echo -e "# Congratulations ZpanelX has now been installed on your     #"
-echo -e "# server. Please review the log file left in /root/ for      #"
-echo -e "# any errors encountered during installation.                #"
-echo -e "#                                                            #"
-echo -e "# Save the following information somewhere safe:             #"
-echo -e "# MySQL Root Password    : $password"
-echo -e "# MySQL Postfix Password : $postfixpassword"
-echo -e "# ZPanelX Username       : zadmin                            #"
-echo -e "# ZPanelX Password       : $zadminNewPass"
-echo -e "#                                                            #"
-echo -e "# ZPanelX Web login can be accessed using your server IP     #"
-echo -e "# inside your web browser.                                   #"
-echo -e "#                                                            #"
-echo -e "##############################################################"
-echo -e ""
+echo -e "##############################################################" &>/dev/tty
+echo -e "# Congratulations ZpanelX has now been installed on your     #" &>/dev/tty
+echo -e "# server. Please review the log file left in /root/ for      #" &>/dev/tty
+echo -e "# any errors encountered during installation.                #" &>/dev/tty
+echo -e "#                                                            #" &>/dev/tty
+echo -e "# Save the following information somewhere safe:             #" &>/dev/tty
+echo -e "# MySQL Root Password    : $password" &>/dev/tty
+echo -e "# MySQL Postfix Password : $postfixpassword" &>/dev/tty
+echo -e "# ZPanelX Username       : zadmin                            #" &>/dev/tty
+echo -e "# ZPanelX Password       : $zadminNewPass" &>/dev/tty
+echo -e "#                                                            #" &>/dev/tty
+echo -e "# ZPanelX Web login can be accessed using your server IP     #" &>/dev/tty
+echo -e "# inside your web browser.                                   #" &>/dev/tty
+echo -e "#                                                            #" &>/dev/tty
+echo -e "##############################################################" &>/dev/tty
+echo -e "" &>/dev/tty
 
 # We now request that the user restarts their server...
 read -e -p "Restart your server now to complete the install (y/n)? " rsn
