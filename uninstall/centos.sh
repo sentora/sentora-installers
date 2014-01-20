a91dd666560dd3340b20f6ea3417e32f088d28e2
Kimpe Andy <andykimpe@gmail.com>
2014-01-20 02:34:55 +0100

    Create centos.sh

---
+++ uninstall/centos.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+yum -y erase ld-linux.so.2 libbz2.so.1 libdb-4.7.so libgd.so.2 httpd php php-suhosin php-devel php-gd php-mbstring php-mcrypt php-intl php-imap php-mysql php-xml php-xmlrpc curl curl-devel perl-libwww-perl libxml2 libxml2-devel mysql-server zip webalizer gcc gcc-c++ httpd-devel at make mysql-devel bzip2-devel postfix postfix-perl-scripts bash-completion dovecot dovecot-mysql dovecot-pigeonhole mysql-server proftpd proftpd-mysql bind bind-utils bind-libs
+
+rm -rf /etc/zpanel
+
+rm -rf /var/zpanel
+
+rm -f /root/passwords.txt
