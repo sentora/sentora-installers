866846c0f90da9ae41ef145225ff6763893bd232
Me-Bl <Me-Bl@users.noreply.github.com>
2014-05-09 11:19:50 +0200

    Update 10_1_1.sh

--- install/CentOS-6_4/10_1_1.sh
+++ install/CentOS-6_4/10_1_1.sh
@@ -68,7 +68,8 @@ fi
 
 # Set custom logging methods so we create a log file in the current working directory.
 logfile=$$.log
-exec >>(tee $logfile)
+touch $$.log
+exec > >(tee $logfile)
 exec 2>&1
 
 # ***************************************
