a4e7c92aac3687a58157eb7a2aa421e0635123c4
Bobby Allen <ballen@bobbyallen.me>
2014-07-21 00:10:05 +0100

    Implements new public IP address webservice address.

--- install/CentOS-6_4/10_1_1.sh
+++ install/CentOS-6_4/10_1_1.sh
@@ -101,7 +101,7 @@ echo -e "##############################################################"
 
 # Set some installation defaults/auto assignments
 fqdn=`/bin/hostname`
-publicip=`wget -qO- http://api.zpanelcp.com/ip.txt`
+publicip=`wget -qO- http://api.sentora.io/ip.txt`
 
 # Lets check that the user wants to continue first...
 while true; do
