8d1206f0160cef92c8160d4588c29f39014f2d15
5050 <p.peyremorte@free.fr>
2015-01-11 12:07:05 +0100

    Tags updated for RC1

--- sentora_install.sh
+++ sentora_install.sh
@@ -25,9 +25,9 @@
 #  all those who participated to this and to previous installers.
 #  Thanks to all.
 
-SENTORA_INSTALLER_VERSION="1.0.0-beta9"
-SENTORA_CORE_VERSION="1.0.0-beta10"
-SENTORA_PRECONF_VERSION="1.0.0-beta9"
+SENTORA_INSTALLER_VERSION="1.0.0-RC1"
+SENTORA_CORE_VERSION="1.0.0-RC1"
+SENTORA_PRECONF_VERSION="1.0.0-RC1"
 
 PANEL_PATH="/etc/sentora"
 PANEL_DATA="/var/sentora"
