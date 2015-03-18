4c6636404c04ec185ecc87de990e0566f91dc6ca
Kimpe Andy <andykimpe@gmail.com>
2015-03-19 00:20:07 +0100

    Create ovh-soyoustar-kimsufi-postinstall.sh

---
+++ ovh-soyoustar-kimsufi-postinstall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# postinstall script for ovh plateform ovh.com soyoustart.com and kimsufi.com
+# temporary accept pull request comment
+wget https://github.com/andykimpe/sentora-installers/raw/master/sentora_install.sh
+# temporary accept pull request uncomment
+#wget https://github.com/sentora/sentora-installers/raw/master/sentora_install.sh
+chmod +x sentora_install.sh
+./sentora_install.sh -t Europe/Paris -d $(hostname) -i public -p 56
