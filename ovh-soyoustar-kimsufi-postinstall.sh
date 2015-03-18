7981bce9cc3135a64ba6bd181fcd876eed9074c2
Kimpe Andy <andykimpe@gmail.com>
2015-03-19 00:57:18 +0100

    Update ovh-soyoustar-kimsufi-postinstall.sh

--- ovh-soyoustar-kimsufi-postinstall.sh
+++ ovh-soyoustar-kimsufi-postinstall.sh
@@ -5,6 +5,12 @@ wget https://github.com/andykimpe/sentora-installers/raw/master/sentora_install.
 # temporary accept pull request uncomment
 #wget https://github.com/sentora/sentora-installers/raw/master/sentora_install.sh
 chmod +x sentora_install.sh
+if [ -f /etc/centos-release ]; then
+yum -y update
+yum -y remove bind
+elif [ -f /etc/lsb-release ]; then
+apt-get update
+apt-get -y dist-upgrade
 ./sentora_install.sh -t Europe/Paris -d $(hostname) -i public -p 56
 echo "OK"
 exit
