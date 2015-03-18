a3ee7f5a906985d900fcf12a9b4044effe877984
Kimpe Andy <andykimpe@gmail.com>
2015-03-19 00:00:29 +0100

    Update suhosin_patch.sh

--- suhosin_patch.sh
+++ suhosin_patch.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 SUHOSIN_VERSION="0.9.37.1"
-cd /tmp
 wget -nv -O suhosin.zip https://github.com/stefanesser/suhosin/archive/$SUHOSIN_VERSION.zip
 unzip -q suhosin.zip
 rm -f suhosin.zip
