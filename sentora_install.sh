2bb7aeb53d6c525c4cae5681fba471d0bc87bc61
Kimpe Andy <andykimpe@gmail.com>
2015-06-21 12:32:04 +0200

    Update sentora_install.sh
    bug fix

--- sentora_install.sh
+++ sentora_install.sh
@@ -667,10 +667,8 @@ mysql -u root -p"$mysqlpassword" -e "DROP DATABASE IF EXISTS test";
 
 # secure SELECT "hacker-code" INTO OUTFILE
 # unsupported configuration for MariaDB 10
-if [[ $OS" = "Fedora" ]] ; then
-echo not edit $MY_CNF_PATH
-else
 # configuration for MySQL 5 and MariaDB 5
+if [[ $OS" = "CentOs" || "$OS" = "Ubuntu" ]] ; then
 sed -i "s|\[mysqld\]|&\nsecure-file-priv = /var/tmp|" $MY_CNF_PATH
 fi
 
