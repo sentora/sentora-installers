f4994cb68b4977cb4065d20a4abdec4267085ce4
Kevin Andrews <kevin@zvps.uk>
2015-09-21 11:54:29 +0100

    Alter all postfix tables to use InnoDB to fix issue: https://github.com/sentora/sentora-installers/issues/53

---
+++ preconf/sentora-update/1-0-4/sql/1-postfix-innodb.sql
@@ -0,0 +1,24 @@
+
+ALTER TABLE `sentora_postfix`.`admin` 
+ENGINE = InnoDB ;
+
+ALTER TABLE `sentora_postfix`.`alias` 
+ENGINE = InnoDB ;
+
+ALTER TABLE `sentora_postfix`.`alias_domain` 
+ENGINE = InnoDB ;
+
+ALTER TABLE `sentora_postfix`.`config` 
+ENGINE = InnoDB ;
+
+ALTER TABLE `sentora_postfix`.`domain` 
+ENGINE = InnoDB ;
+
+ALTER TABLE `sentora_postfix`.`mailbox` 
+ENGINE = InnoDB ;
+
+ALTER TABLE `sentora_postfix`.`quota2` 
+ENGINE = InnoDB ;
+
+ALTER TABLE `sentora_postfix`.`vacation` 
+ENGINE = InnoDB ;
