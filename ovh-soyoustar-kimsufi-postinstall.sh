2115e65405f3ebb5c88b23da6098cad9a9ab2ea3
amidevous <amidevous@users.noreply.github.com>
2024-11-26 12:40:51 +0100

    Update ovh-soyoustar-kimsufi-postinstall.sh

--- ovh-soyoustar-kimsufi-postinstall.sh
+++ ovh-soyoustar-kimsufi-postinstall.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 # postinstall script for ovh plateform ovh.com soyoustart.com and kimsufi.com
+#for centos 6 centos 7 ubuntu old
 wget http://sentora.org/install -O sentora_install.sh
 chmod +x sentora_install.sh
 if [ -f /etc/centos-release ]; then
 yum -y update
 yum -y remove bind
 elif [ -f /etc/lsb-release ]; then
+export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get -y dist-upgrade
+DEBIAN_FRONTEND=noninteractive apt-get -y dist-upgrade
 fi
 ./sentora_install.sh -t Europe/Paris -d $(hostname) -i public
 echo "OK"
